767c937f2de251158895cce3e2291fab6e8934c9
abdulbahajaj <abdulbahajaj@gmail.com>
2020-08-26 22:43:49 -0600

    Enabling non-json responses

--- vserver/src/vserver/runtime.clj
+++ vserver/src/vserver/runtime.clj
@@ -5,15 +5,16 @@
 (def server-funcs (atom {}))
 
 (defmacro defs [name args & rest]
-    `(swap! server-funcs
-            assoc
-            (format "/server-functions/%s" ~(str name))
-            (fn [request#]
-              {:status 200
-               :body
-               (let [body# (:body request#)]
-                 (apply (fn [~@args] (do ~@rest))
-                        (map #(% body#) ~(map #(keyword %) args))))})))
+  `(swap! server-funcs
+          assoc
+          (format "/server-functions/%s" ~(str name))
+          (fn [request#]
+            {:status 200
+             :body
+             {:response
+              (let [body# (:body request#)]
+                (apply (fn [~@args] (do ~@rest))
+                       (map #(% body#) ~(map #(keyword %) args)))) }})))
 
 (defn get-server-function [path]
   (get @server-funcs path))
