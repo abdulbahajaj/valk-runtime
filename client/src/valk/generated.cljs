a9bde3753975755f5ca1997c89e1ca9a531f7418
abdulbahajaj <abdulbahajaj@gmail.com>
2020-08-30 00:42:22 -0600

    Client macro to ignore startd

--- client/src/valk/generated.cljs
+++ client/src/valk/generated.cljs
@@ -1,5 +1,5 @@
 (ns valk.generated
-  (:require-macros [valk.runtime :refer [defs]])
+  (:require-macros [valk.runtime :refer [defs startd]])
   (:require [reagent.core :as r]
             [valk.runtimejs :as rnjs]))
 
--- client/src/valk/runtime.clj
+++ client/src/valk/runtime.clj
@@ -6,3 +6,5 @@
       ~(format "server-functions/%s" name)
       {:body (hash-map ~@(flatten (map #(do [(keyword %) %]) args)))}
       callback#)))
+
+(defmacro startd [name interval & rest])
